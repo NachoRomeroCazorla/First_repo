0606060a3f670f7dcfa77eb72b58fc7587025919
NachoRomeroCazorla <ignacioromerocazorla@gmail.com>
2023-05-03 10:57:15 +0200

    Upload a folder with python environment and code

---
+++ Python/test1.py
@@ -0,0 +1,13 @@
+#This is just a test to add to the GitHub repo
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+a = np.linspace(0, 10, 50)
+b = np.sin(a)
+print(a)
+plt.figure()
+plt.plot(a , b)
+plt.show()
+plt.savefig("Figure.jpg")
