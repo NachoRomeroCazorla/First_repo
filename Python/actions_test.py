1a9fa945ce57e991e97d187db1f0f9ff75db980e
NachoRomeroCazorla <ignacioromerocazorla@gmail.com>
2023-05-08 12:19:59 +0200

    Rewrite py file

--- Python/actions_test.py
+++ Python/actions_test.py
@@ -1,7 +1,5 @@
 string = "The github actions has been executed properly :)"
-file = open("test_GHActions.txt","w") 
-archi1 = open("datos.txt","w") 
-archi1.write(string)
-archi1.close() 
-
+file = open("Python/test_GHActions.txt","w") 
+file.write(string)
+file.close() 
 
