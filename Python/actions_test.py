3e6c488f8dd011688bf314f80cbc177e8694f544
NachoRomeroCazorla <ignacioromerocazorla@gmail.com>
2023-05-08 12:52:55 +0200

    Changes in file directory

--- Python/actions_test.py
+++ Python/actions_test.py
@@ -1,5 +1,5 @@
 string = "The github actions has been executed properly :)"
-file = open("Python/test_GHActions.txt","w") 
+file = open("test_GHActions.txt","w") 
 file.write(string)
 file.close() 
 
